ede29e45485d68af7bb0044f26808d72ff07c3f3
linzhichao <zhichao.lin@ele.me>
2017-08-30 00:37:21 +0800

    NonDecreasingArray

--- src/main/java/cn/hackcoder/NonDecreasingArray.java
+++ src/main/java/cn/hackcoder/NonDecreasingArray.java
@@ -5,23 +5,15 @@ package cn.hackcoder;
  */
 public class NonDecreasingArray {
     public boolean checkPossibility(int[] nums) {
-        int before = Integer.MIN_VALUE;
-        int index = -1;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < before) {
-                if (index != -1) return false;
-                index = i;
-            }
-            before = nums[i];
-        }
-        if (index > 0 && index < nums.length - 1) {
-            for (int i = 0; i < index; i++) {
-                if (nums[index + 1] < nums[i]) {
-                    return false;
-                }
+        int count = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] > nums[i]) {
+                count++;
+                if (i - 2 < 0 || nums[i - 2] < nums[i]) nums[i - 1] = nums[i];
+                else nums[i] = nums[i - 1];
             }
         }
-        return true;
+        return count <= 1;
     }
 
     public static void main(String[] args) {
