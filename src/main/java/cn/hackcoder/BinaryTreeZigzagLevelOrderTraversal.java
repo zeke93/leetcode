d06b6f71c40947df7909ccca717f1b608131142b
linzhichao <hackcode93@gmail.com>
2018-08-30 23:33:20 +0800

    BinaryTreeZigzagLevelOrderTraversal

---
+++ src/main/java/cn/hackcoder/BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,25 @@
+package cn.hackcoder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 103. Binary Tree Zigzag Level Order Traversal
+ */
+public class BinaryTreeZigzagLevelOrderTraversal {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> results = new ArrayList<>();
+
+        return results;
+    }
+}
