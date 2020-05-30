f6053a543cbb25cd84d420d8e2ed1c97854c1b27
linzhichao <linzhichao@corp.netease.com>
2020-05-31 01:29:25 +0800

    #64

--- src/main/java/cn/hackcoder/MinimumPathSum.java
+++ src/main/java/cn/hackcoder/MinimumPathSum.java
@@ -3,34 +3,43 @@ package cn.hackcoder;
 import java.util.Random;
 
 /**
- * Created by linzhichao on 16/10/24.
+ * 64. Minimum Path Sum
  */
 public class MinimumPathSum {
-    /**
-     * Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right
-     * which minimizes the sum of all numbers along its path.
-     * Note: You can only move either down or right at any point in time.
-     *
-     * @param grid
-     * @return
-     */
-    public int minPathSum(int[][] grid) {
-        return 0;
-    }
 
-    public static void main(String[] args) {
-        int n = 10;
-        int[][] grid = new int[10][10];
-        Random random = new Random();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                grid[i][j] = random.nextInt(10);
-                System.out.println(grid[i][j] + "   ");
-            }
-            System.out.println();
-        }
+	public int minPathSum(int[][] grid) {
+		int m = grid.length;
+		int n = grid[0].length;
+		int[][] map = new int[m][n];
+		map[0][0] = grid[0][0];
+		for (int i = 1; i < m; i++) {
+			map[i][0] = map[i - 1][0] + grid[i][0];
+		}
+		for (int j = 1; j < n; j++) {
+			map[0][j] = map[0][j - 1] + grid[0][j];
+		}
 
-        new MinimumPathSum().minPathSum(grid);
-    }
+		for (int i = 1; i < m; i++) {
+			for (int j = 1; j < n; j++) {
+				map[i][j] = Math.min(map[i - 1][j], map[i][j - 1]) + grid[i][j];
+			}
+		}
+		return map[m - 1][n - 1];
+	}
+
+	public static void main(String[] args) {
+		int n = 4;
+		int[][] grid = new int[n][n];
+		Random random = new Random();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				grid[i][j] = random.nextInt(3);
+				System.out.print(grid[i][j] + "   ");
+			}
+			System.out.println();
+		}
+
+		System.out.println(new MinimumPathSum().minPathSum(grid));
+	}
 
 }
