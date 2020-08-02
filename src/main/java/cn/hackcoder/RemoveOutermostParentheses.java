9439f69c9d49caab5274153f989650425bb21175
linzhichao <linzhichao@corp.netease.com>
2020-08-02 09:33:52 +0800

    1021. Remove Outermost Parentheses

--- src/main/java/cn/hackcoder/RemoveOutermostParentheses.java
+++ src/main/java/cn/hackcoder/RemoveOutermostParentheses.java
@@ -1,29 +1,44 @@
 package cn.hackcoder;
 
+import java.util.Stack;
+
 /**
  * 1021. Remove Outermost Parentheses
  */
 public class RemoveOutermostParentheses {
 	public String removeOuterParentheses(String S) {
-		String s = "";
-		int l = 0;
-		int leftCnt = 0;
-		int rightCnt = 0;
-		for (int i = 0; i < S.length(); i++) {
-			if (S.charAt(i) == '(') {
-				leftCnt++;
+		StringBuilder sb = new StringBuilder();
+		Stack<Character> stack = new Stack<>();
+		int left = 0;
+		int right = 0;
+		for (char c : S.toCharArray()) {
+			if (c == ')') {
+				right++;
+				if (left == 1) {
+					stack.pop();
+					left--;
+					right--;
+				} else if (left == right) {
+					StringBuilder s = new StringBuilder();
+					while (!stack.isEmpty()) {
+						s.append(stack.pop());
+					}
+					s.deleteCharAt(s.length() - 1);
+					sb.append(s.reverse());
+				} else {
+					stack.push(c);
+				}
 			} else {
-				rightCnt++;
-			}
-			if (leftCnt == rightCnt) {
-				s += S.substring(l + 1, i);
-				l = i+1;
+				left++;
+				stack.push(c);
 			}
 		}
-		return s;
+		return sb.toString();
 	}
 
 	public static void main(String[] args) {
+		System.out.println(new RemoveOutermostParentheses().removeOuterParentheses("(()())(())"));
+		System.out.println(new RemoveOutermostParentheses().removeOuterParentheses("(()())(())(()(()))"));
 		System.out.println(new RemoveOutermostParentheses().removeOuterParentheses("()()"));
 	}
 }
